957617d557a80dc502721c517845b3076f3bf0bf
Alex Hagen <ahagen@purdue.edu>
2017-05-09 09:18:55 -0400

    got a reasonable preamble for the mathjax extensions, but it wont export on nb_convert, right now

---
+++ __init__.py
@@ -0,0 +1,10 @@
+from IPython.display import display, Javascript
+
+display(Javascript("""
+MathJax.Hub.Insert(MathJax.InputJax.TeX.Definitions.macros,{
+    cancel: ["Extension","cancel"],
+    bcancel: ["Extension","cancel"],
+    xcancel: ["Extension","cancel"],
+    cancelto: ["Extension","cancel"]
+  });
+"""))
