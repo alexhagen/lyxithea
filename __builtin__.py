25ff39295895b424ed8eaadc6bdee878ea9c00de
Alex Hagen <ahagen@purdue.edu>
2017-05-18 09:33:55 -0400

    working dissertation magic, first cut

--- __builtin__.py
+++ __builtin__.py
@@ -2,3 +2,5 @@ __labels__ = {}
 __tabcount__ = 1
 __tables__ = {}
 __nom__ = {'abbr': {}, 'unit': {}, 'supsub': {}}
+
+__cdis__ = None
--- dissertation.py
+++ dissertation.py
@@ -1,5 +1,8 @@
 import datetime
 from IPython.display import display, HTML
+from IPython.core.magic import (Magics, magics_class, line_magic,
+                                cell_magic, line_cell_magic)
+import __builtin__ as bi
 
 class document(object):
     """ A document class """
@@ -14,61 +17,107 @@ class dissertation(document):
         now = datetime.datetime.now()
         self._month = now.strftime("%B")
         self._year = now.strftime("%Y")
+        self._chapter_paths = []
+        self._data_paths = []
+        bi.__cdis__ = self
         super(dissertation, self).__init__()
+
+    @staticmethod
+    def check_chapter_path(path):
+        """ checks if the chapter path is a legitimate path
+
+        :param path: the path to the folder to be added
+        :returns: boolean if the path is good or not
+        """
+        return true
+
+    @staticmethod
+    def process_markdown(markdown):
+        """ looks for python parameter notation in a markdown string """
+        return markdown
+
+    def chapter_paths(self, paths):
+        """ add paths to the chapter path variable
+
+        :param paths: a list of string paths in the filesystem where chapters
+            are found
+        """
+        for path in paths:
+            if self.check_chapter_path(path):
+                self._chapter_paths.extend([path])
+        return self
 
     def title(self, title):
         self._title = title
+        return self
 
     def author(self, author):
         self._author = author
+        return self
 
     def author_backwards(self, author):
         self._author_backwards = author
+        return self
 
     def degree(self, degree):
         self._degree = degree
+        return self
 
     def dgr(self, dgr):
         self._dgr = dgr
+        return self
 
     def major_prof(self, major_prof):
         self._major_prof = major_prof
+        return self
 
     def campus(self, campus):
         self._campus = campus
+        return self
 
     def dedication(self, dedication):
-        self._dedication = dedication
+        self._dedication = self.process_markdown(dedication)
+        return self
 
     def acknowledgements(self, ack):
-        self._acknowledgements = ack
+        self._acknowledgements = self.process_markdown(ack)
+        return self
 
     def preface(self, preface):
-        self._preface = preface
+        self._preface = self.process_markdown(preface)
+        return self
 
     def toc(self):
         pass
+        return self
 
     def lot(self):
         pass
+        return self
 
     def lof(self):
         pass
+        return self
 
     def nom(self):
         pass
+        return self
 
     def abstract(self, abstract):
-        self._abstract = abstract
+        self._abstract = self.process_markdown(abstract)
+        return self
 
     def chapter(self, filename):
         pass
+        return self
 
     def appendix(self, filename):
         pass
+        return self
 
     def bibliography(self):
         self._bib.bibliography()
+        return self
 
     def peek(self):
         html_str = """<h1>{title}</h1>
@@ -77,3 +126,20 @@ class dissertation(document):
         """.format(title=self._title, author=self._author, month=self._month,
                    year=self._year)
         display(HTML(html_str))
+        return self
+
+ip = get_ipython()
+
+@magics_class
+class dissertation_magics(Magics):
+
+    @line_cell_magic
+    def dis(self, line, cell):
+        """ call function of dissertation from a line magic """
+        cdis = bi.__cdis__
+        cmd_str = "cdis.{method}(\"\"\"{arg}\"\"\")" \
+            .format(method=line, arg=cell)
+        print cmd_str
+        exec(cmd_str)
+
+ip.register_magics(dissertation_magics)
