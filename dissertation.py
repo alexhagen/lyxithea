d51caed7d137df4077dd8388ab87a21129b4625e
Alex Hagen <ahagen@purdue.edu>
2017-05-18 08:30:45 -0400

    added peek and month stuff

--- dissertation.py
+++ dissertation.py
@@ -1,3 +1,5 @@
+import datetime
+
 class document(object):
     """ A document class """
     def __init__(self):
@@ -8,6 +10,9 @@ class dissertation(document):
     """ A dissertation class """
     def __init__(self, bib):
         self._bib = bib
+        now = datetime.datetime.now()
+        self._month = now.strftime("%B")
+        self._year = now.strftime("%Y")
         super(dissertation, self).__init__()
 
     def title(self, title):
@@ -30,3 +35,10 @@ class dissertation(document):
 
     def campus(self, campus):
         self._campus = campus
+
+    def peek(self):
+        html_str = """<h1>{title}</h1>
+        <h2>{author}</h2>
+        <p>{month}, {year}</p>
+        """.format(title=self._title, author=self._author, month=self._month,
+                   year=self._year)
