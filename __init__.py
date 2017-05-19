8c4b66cb123c6af0044676e24bb83eb69d5dd377
Alex Hagen <ahagen@purdue.edu>
2017-05-18 20:54:11 -0400

    trying the simpler ce and unit defs

--- __init__.py
+++ __init__.py
@@ -6,24 +6,12 @@ MathJax.Hub.Register.StartupHook("TeX Jax Ready",function () {
       TeX = MathJax.InputJax.TeX;
 
   TeX.Definitions.macros.nicefrac = "myBevelFraction";
-  TeX.Definitions.macros.unit = "myUnit";
-  TeX.Definitions.macros.ce = "myCe";
 
   TeX.Parse.Augment({
     myBevelFraction: function (name) {
       var num = this.ParseArg(name),
           den = this.ParseArg(name);
       this.Push(MML.mfrac(num,den).With({bevelled: true}));
-    },
-    myUnit: function(name) {
-      var num = this.ParseArg(name);
-      var den = 2;
-      this.Push(MML.mtext(num).With({variant: 'normal'}));
-    },
-    myCe: function(name) {
-      var num = this.ParseArg(name);
-      var den = 2;
-      this.Push(MML.mtext(num).With({variant: 'normal'}));
     }
   });
   MathJax.Hub.Insert(MathJax.InputJax.TeX.Definitions.macros,{
@@ -34,6 +22,9 @@ MathJax.Hub.Register.StartupHook("TeX Jax Ready",function () {
     });
 });
 """))
+
+display(HTML(r'$\newcommand{\unit}[1]{\mathrm{#1}}$'))
+display(HTML(r'$\newcommand{\ce}[1]{\mathrm{#1}}$'))
 
 display(HTML('<link rel="stylesheet" href="https://cdn.rawgit.com/tiaanduplessis/wenk/master/dist/wenk.css">'))
 display(HTML('<style> span.abbr{ color: #555; } </style>'))
