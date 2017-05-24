fe3ffea4b77bb32156da2cefa4b0ef7086c505d6
Alex Hagen <ahagen@purdue.edu>
2017-05-24 11:16:27 -0400

    sort of working markdown code hiding

--- __init__.py
+++ __init__.py
@@ -28,5 +28,37 @@ def header():
     html_str += '<link rel="stylesheet" href="https://cdn.rawgit.com/tiaanduplessis/wenk/master/dist/wenk.css">\n'
     html_str += '<style> span.abbr{ color: #999; } </style>\n<style> [data-wenk]:after { z-index: 1000000000; } </style>\n'
     display(HTML(html_str))
+    javascript_str = """
+        $('.celltoolbar > .button_container').children('.toggleButton').remove()
+
+        var toggle_button = ("<button class='toggleButton' type='button' value='true'>Show Code</button>");
+        $('.celltoolbar > .button_container').append(toggle_button);
+
+        function code_toggle()
+        {
+            var code_shown = $(this).attr('value') == 'true';
+            if (code_shown)
+            {
+                console.log("code shown")
+                $(this).parent().parent().parent().parent().children('.input_area').hide('500');
+                $(this).text('Show Code');
+            }
+            else
+            {
+                console.log("code not shown")
+                $(this).parent().parent().parent().parent().children('.input_area').show('500');
+                $(this).text('Hide Code');
+            }
+
+            code_shown = !code_shown;
+            $(this).attr('value', code_shown);
+        }
+
+
+        $('.toggleButton').each(function() {
+            $(this).on('click', code_toggle);
+        });
+    """
+    display(Javascript(javascript_str))
 
 header()
